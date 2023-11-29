b67523079ec7d34ab4ab89e39a546c4a1bbe2cd4
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2023-11-29 20:57:18 +0800

    cli testing

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const prompts = require('prompts');
+
+const questions = [
+  {
+    type: 'text',
+    name: 'username',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'number',
+    name: 'age',
+    message: 'How old are you?'
+  },
+  {
+    type: 'text',
+    name: 'about',
+    message: 'Tell something about yourself',
+    initial: 'Why should I?'
+  }
+];
+
+(async () => {
+  const response = await prompts(questions);
+
+  // => response => { username, age, about }
+})();
